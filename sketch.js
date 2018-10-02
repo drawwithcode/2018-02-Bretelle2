a2287f61d95cd37f07127f2a040830b90e04806d
Francesco Longoni <fra-long96@hotmai.it>
2018-10-02 20:06:52 +0200

    non sono sicuro sia corretto quello che ho fatto

--- sketch.js
+++ sketch.js
@@ -1,11 +1,34 @@
-function preload(){
-  // put preload code here
-}
+var canvas;
 
 function setup() {
-  // put setup code here
+  canvas = createCanvas(windowWidth, windowHeight);
+  // smooth();
+  angleMode(DEGREES);
+  frameRate(30);
+  noCursor();
+  noFill();
 }
 
+var a;
+
+var movx;
+var movy;
+
 function draw() {
-  // put drawing code here
+
+background(22, 42, 62);
+stroke(255);
+strokeWeight(2);
+ellipse(mouseX, mouseY, 25);
+
+for (a=0; a<360; a=a+12){
+  // movx = cos(a*frameCount);
+  // movy = sin(a*frameCount);
+ line(mouseX+cos(a + frameCount)*20,mouseY+sin(a + frameCount)*20, mouseX+cos(a + frameCount)*50,mouseY+sin(a + frameCount)*50);
+
+ bezier(mouseX, mouseY, cos(frameCount)*50,sin(frameCount)*50, cos(frameCount)*50,sin(frameCount)*50, 0,0 )
+ bezier(mouseX, mouseY, cos(frameCount)*50,sin(frameCount)*50, cos(frameCount)*50,sin(frameCount)*50, sin(frameCount)*width,0 )
+ bezier(mouseX, mouseY, cos(frameCount)*50,sin(frameCount)*50, cos(frameCount)*50,sin(frameCount)*50, 0,sin(frameCount)*height )
+ bezier(mouseX, mouseY, cos(frameCount)*50,sin(frameCount)*50, cos(frameCount)*50,sin(frameCount)*50, width, sin(frameCount)*height )
+}
 }
